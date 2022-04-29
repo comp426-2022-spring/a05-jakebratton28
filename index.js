1cc90065a6f44b714a35db06657e0bd18e80c2c1
jacob28 <jacob28@live.unc.edu>
2022-04-29 19:46:38 -0400

    small update again

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use( (req, res, next) => {
 })
 
 
-// Oh look a
+// Oh look
 if (debug) {
     // Returns all records in the 'accesslog' table
     app.get('/app/log/access', (req, res, next) => {
